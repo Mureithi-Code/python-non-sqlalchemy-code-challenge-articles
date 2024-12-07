6de53a59dc7e9f188f14ee49515ee4e425b87ef1
Joseph <darkknight505c@gmail.com>
2024-12-07 13:17:45 +0300

    last update

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,3 +1,52 @@
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
+            raise ValueError("Title must be a string between 5 and 50 characters.")
+        if not isinstance(author, Author):
+            raise ValueError("Author must be an instance of the Author class.")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be an instance of the Magazine class.")
+
+        self._title = title
+        self._author = author
+        self._magazine = magazine
+        Article.all.append(self)
+
+        # Automatically add the article to the author and magazine
+        author._articles.append(self)
+        magazine.add_article(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Cannot change article title once set.")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise ValueError("Author must be an instance of the Author class.")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise ValueError("Magazine must be an instance of the Magazine class.")
+        self._magazine = value
+
+
 class Author:
     def __init__(self, name):
         if not isinstance(name, str) or len(name) == 0:
@@ -106,52 +155,3 @@ class Magazine:
                 top_magazine = magazine
 
         return top_magazine
-
-
-class Article:
-    all = []
-
-    def __init__(self, author, magazine, title):
-        if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
-            raise ValueError("Title must be a string between 5 and 50 characters.")
-        if not isinstance(author, Author):
-            raise ValueError("Author must be an instance of the Author class.")
-        if not isinstance(magazine, Magazine):
-            raise ValueError("Magazine must be an instance of the Magazine class.")
-
-        self._title = title
-        self._author = author
-        self._magazine = magazine
-        Article.all.append(self)
-
-        # Automatically add the article to the author and magazine
-        author._articles.append(self)
-        magazine.add_article(self)
-
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, value):
-        raise AttributeError("Cannot change article title once set.")
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise ValueError("Author must be an instance of the Author class.")
-        self._author = value
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, value):
-        if not isinstance(value, Magazine):
-            raise ValueError("Magazine must be an instance of the Magazine class.")
-        self._magazine = value
